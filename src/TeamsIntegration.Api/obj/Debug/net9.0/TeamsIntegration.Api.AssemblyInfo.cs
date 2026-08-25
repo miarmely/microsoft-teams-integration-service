3ff77f2651460a524145f2d8fd9bc8b4c4cecff6
mert akdemir <mert.akdemir@mksdevo.com>
2026-08-25 09:48:10 +0300

    Stable Teams Integration Service v1. / @miarmely

--- src/TeamsIntegration.Api/obj/Debug/net9.0/TeamsIntegration.Api.AssemblyInfo.cs
+++ src/TeamsIntegration.Api/obj/Debug/net9.0/TeamsIntegration.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamsIntegration.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eda323fa63a1db600afd8ca7052de5f29a2094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ebe152db9269f0653e12506a6e8873706f7d3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamsIntegration.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamsIntegration.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
